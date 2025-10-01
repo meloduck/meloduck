61018657953bcbae3cb0804d6fd5e0edb8565e97
meloduck <tijnvanraalte@gmail.com>
2025-10-01 10:58:49 +0200

    guh

---
+++ dice roll.py
@@ -0,0 +1,17 @@
+#dice roll
+
+#to do:
+    #
+
+import random
+
+#number of sides (max):
+max=20
+min=1
+
+#amount of die rolled (rolls):
+rolls=2
+
+for rolls in range(rolls):
+    roll=random.randint(min,max)
+    print(f"You rolled {roll}!")
