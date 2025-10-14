907eb138de4542ca519a97086a1b95a11ce6e0d2
meloduck <tijnvanraalte@gmail.com>
2025-10-14 11:22:41 +0200

    Update standard galactic alphabet translator.py
    made the actual translator.
    the letters H and V are weird idk how to fix that.

--- standard galactic alphabet translator.py
+++ standard galactic alphabet translator.py
@@ -1,35 +1,45 @@
 #standard galactic alphabet translator 
 
 #to do:
-    #make translator
+    #
 
-#SGA dictionary
+#Standard Galactic Alphabet dictionary
 sgaDict={
-    "A":"ᔑ",
-    "B":"ʖ",
-    "C":"ᓵ",
-    "D":"↸",
-    "E":"ᒷ",
-    "F":"⎓",
-    "G":"⊣",
-    "H":"⍑",
-    "I":"╎",
-    "J":"⋮",
-    "K":"ꖌ",
-    "L":"ꖎ",
-    "M":"ᒲ",
-    "N":"リ",
-    "O":"𝙹",
-    "P":"!¡",
-    "Q":"ᑑ",
-    "R":"∷",
-    "S":"ᓭ",
-    "T":"ℸ.",
-    "U":"⚍",
-    "V":"⍊",
-    "W":"∴",
-    "X":"̇/",
-    "Y":"||",
-    "Z":"⨅"} 
+    "A":"ᔑ ",
+    "B":"ʖ ",
+    "C":"ᓵ ",
+    "D":"↸ ",
+    "E":"ᒷ ",
+    "F":"⎓ ",
+    "G":"⊣ ",
+    "H":"⍑ ",
+    "I":"╎ ",
+    "J":"⋮ ",
+    "K":"ꖌ ",
+    "L":"ꖎ ",
+    "M":"ᒲ ",
+    "N":"リ ",
+    "O":"𝙹 ",
+    "P":"!¡ ",
+    "Q":"ᑑ ",
+    "R":"∷ ",
+    "S":"ᓭ ",
+    "T":"ℸ. ",
+    "U":"⚍ ",
+    "V":"⍊ ",
+    "W":"∴ ",
+    "X":"̇/ ",
+    "Y":"|| ",
+    "Z":"⨅ "}
 
 #translator
+userinput=input("What would you like to be translated?: ")
+translation=""
+for char in userinput.upper():
+    if char in sgaDict:
+        translation+=sgaDict[char]
+    elif char=="\\":
+        translation+="\n"
+    else:
+        translation+=char
+print(f"Translation: {translation}")
